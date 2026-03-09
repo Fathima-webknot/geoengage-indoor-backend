b7c54e0c97ec0e875227a834115a631c87d0c291
aman-prakash-1222 <aman.prakash@webknot.in>
2026-03-09 16:00:23 +0530

    fix: create_engine parameters are updated

--- app/db/session.py
+++ app/db/session.py
@@ -4,8 +4,19 @@ from sqlalchemy.orm import sessionmaker
 from app.config import settings
 from app.db.base import Base
 
-engine = create_engine(settings.database_url, pool_pre_ping=True)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = create_engine(
+    settings.database_url,
+    pool_pre_ping=True,
+    pool_size=20,
+    max_overflow=20,
+    pool_timeout=30
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
 
 def get_db():
